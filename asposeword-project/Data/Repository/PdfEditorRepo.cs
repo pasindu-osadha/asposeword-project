4eda2deeeffb5edbd01a417cad093e2d902cf900
pasindu-osadha <pasiduosanda@gmail.com>
2022-05-18 11:10:54 +0530

    Add Header section

--- asposeword-project/Data/Repository/PdfEditorRepo.cs
+++ asposeword-project/Data/Repository/PdfEditorRepo.cs
@@ -19,59 +19,74 @@ namespace asposeword_project.Data.Repository
             // Add page
             Page page = document.Pages.Add();
 
-
-            // Add Header
-            var header = new TextFragment("Allion Technologies - Audit result");
-            header.TextState.Font = FontRepository.FindFont("Arial");
-            header.TextState.FontSize = 20;
-            header.HorizontalAlignment = HorizontalAlignment.Center;
-            header.Position = new Position(100, 800);
-            HeaderFooter h = new HeaderFooter();
-            h.Paragraphs.Add(header);
-            page.Header = h;
-
-            //Header table 
+            //Add header 
             var headerTable = new Table
             {
                 DefaultCellPadding = new MarginInfo(4.5, 4.5, 4.5, 4.5),
                 Margin =
                 {
+                    Top= 20
+                }
+            };
+            headerTable.ColumnAdjustment = ColumnAdjustment.AutoFitToWindow;
+            var headerTable_Row = headerTable.Rows.Add();
+            headerTable_Row.Cells.Add(DateTime.Now.ToString());
+            headerTable_Row.Cells.Add("HCSL Quality Assurance system");
+            HeaderFooter headerFooter = new HeaderFooter();
+            headerFooter.Paragraphs.Add(headerTable);
+            page.Header = headerFooter;
+
+
+
+            //Title 
+            var title = new TextFragment("Allion Technologies - Audit result");
+            title.TextState.FontSize = 20;
+            title.HorizontalAlignment = HorizontalAlignment.Center;
+            page.Paragraphs.Add(title);
+
+
+            //Header table 
+            var headerInformationTable = new Table
+            {
+                DefaultCellPadding = new MarginInfo(4.5, 4.5, 4.5, 4.5),
+                Margin =
+                {   
                     Bottom = 20
                 }
             };
-            headerTable.ColumnWidths = "150 150 150";
+            headerInformationTable.ColumnWidths = "150 150 150";
 
-            var codeName_Row = headerTable.Rows.Add();
+            var codeName_Row = headerInformationTable.Rows.Add();
             codeName_Row.Cells.Add("Code");
             var code_Name_Row_cell = codeName_Row.Cells.Add("Name");
             code_Name_Row_cell.ColSpan = 2;
 
-            var codeName_Data_Row = headerTable.Rows.Add();
+            var codeName_Data_Row = headerInformationTable.Rows.Add();
             codeName_Data_Row.Cells.Add(Dto.Code); // API 
             var codeName_Data_Row_cell = codeName_Data_Row.Cells.Add(Dto.Name); // API
             codeName_Data_Row_cell.ColSpan = 2;
 
 
-            var description_Row = headerTable.Rows.Add();
+            var description_Row = headerInformationTable.Rows.Add();
             var description_Row_cell = description_Row.Cells.Add("Description");
             description_Row_cell.ColSpan = 3;
 
-            var description_Row_Data = headerTable.Rows.Add();
+            var description_Row_Data = headerInformationTable.Rows.Add();
             var description_Row_Data_cell = description_Row_Data.Cells.Add(Dto.Description); // API
             description_Row_Data_cell.ColSpan = 3;
 
 
-            var completedBY_Row = headerTable.Rows.Add();
+            var completedBY_Row = headerInformationTable.Rows.Add();
             completedBY_Row.Cells.Add("Date Taken");
             completedBY_Row.Cells.Add("Completed by");
             completedBY_Row.Cells.Add("Designation");
 
-            var completedBY_Data_Row = headerTable.Rows.Add();
+            var completedBY_Data_Row = headerInformationTable.Rows.Add();
             completedBY_Data_Row.Cells.Add(Dto.DateTaken.ToString()); //API
             completedBY_Data_Row.Cells.Add(Dto.CompletedBy); //API
             completedBY_Data_Row.Cells.Add(Dto.Designation); //API
 
-            page.Paragraphs.Add(headerTable);
+            page.Paragraphs.Add(headerInformationTable);
 
 
             //Data table 
@@ -97,11 +112,11 @@ namespace asposeword_project.Data.Repository
 
             //Image image = new Image();
             //image.File = "Files\\imageFile\\yes.png";
-            
 
-            for (int i = 0; i <Dto.tableData.Count; i++)
+
+            for (int i = 0; i < Dto.tableData.Count; i++)
             {
-              
+
 
                 var dataTable_DataRow = dataTable.Rows.Add();
                 dataTable_DataRow.Cells.Add(Dto.tableData[i].Criteria);//API
@@ -110,13 +125,17 @@ namespace asposeword_project.Data.Repository
 
                 switch (Dto.tableData[i].Completed)
                 {
-                    case 0: img.File = "Files//imageFile//no.png";
+                    case 0:
+                        img.File = "Files//imageFile//no.png";
                         break;
-                    case 1: img.File = "Files//imageFile//yes.png";
+                    case 1:
+                        img.File = "Files//imageFile//yes.png";
                         break;
-                    case 2: img.File = "Files//imageFile//undefine.png";
+                    case 2:
+                        img.File = "Files//imageFile//undefine.png";
                         break;
-                    default : img.File = "Files//imageFile//undefine.png";
+                    default:
+                        img.File = "Files//imageFile//undefine.png";
                         break;
                 }
 
@@ -124,7 +143,7 @@ namespace asposeword_project.Data.Repository
                 img.FixWidth = 10;
                 var c = dataTable_DataRow.Cells.Add();
                 c.Paragraphs.Add(img);
-                
+
                 dataTable_DataRow.Cells.Add(Dto.tableData[i].Findir);
                 dataTable_DataRow.Cells.Add(Dto.tableData[i].Response);
                 dataTable_DataRow.Cells.Add(Dto.tableData[i].Corrective);
